f0f769726e958ca0acdee26b88f87bddaa291f30
unknown <danield@JEM1.edh.local>
2015-05-08 06:30:51 -0700

    Fixed object variable error

--- scripts/battleship.js
+++ scripts/battleship.js
@@ -33,8 +33,8 @@ var view = {
 var model = {
 	boardSize: 7,
 	numShips: 3,
-	shipLength; 3,
-	shipsSunk 0,
+	shipLength: 3,
+	shipsSunk: 0,
 	
 	ships: [{locations:["06","16","26"], hits: ["","",""]},
 			{locations:["24","34","44"], hits: ["","",""]},
