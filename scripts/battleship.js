1a549758fc77e201ce9432ab4d518ab08d763bc2
Daniel <dduffy2@outlook.com>
2015-05-09 19:35:11 -0700

    add event handlers and generation logic. Game is fully operational

--- scripts/battleship.js
+++ scripts/battleship.js
@@ -33,12 +33,12 @@ var view = {
 var model = {
 	boardSize: 7,
 	numShips: 3,
-	shipLength; 3,
-	shipsSunk 0,
+	shipLength: 3,
+	shipsSunk: 0,
 	
-	ships: [{locations:["06","16","26"], hits: ["","",""]},
-			{locations:["24","34","44"], hits: ["","",""]},
-			{locations:["10","11","12"], hits: ["","",""]}],
+	ships: [{locations:[0,0,0], hits: ["","",""]},
+			{locations:[0,0,0], hits: ["","",""]},
+			{locations:[0,0,0], hits: ["","",""]}],
 			
 	fire: function(guess){
 		
@@ -49,9 +49,19 @@ var model = {
 			if(index >= 0){
 				//We have a hit!
 				ship.hits[index] = "hit";
+				view.displayHit(guess);
+				view.displayMessage("HIT!!!");
+				
+				if(this.isSunk(ship)){
+					view.displayMessage("You Sank my battleship!");
+					this.shipsSunk++;
+				}
 				return true;
 			}
 		}
+		view.displayMiss(guess);
+		view.displayMessage("You Missed.");
+		
 		return false;
 	},
 	
@@ -62,9 +72,60 @@ var model = {
 			}
 		}
 		return true;
+	},
+	
+	generateShipLocations: function(){
+		var locations;
+		for(var i = 0; i < this.numShips; i++){
+			
+			do{
+				locations = this.generateShip();
+				
+			}while(this.collision(locations));
+			this.ships[i].locations = locations;
+		}
+	},
+	
+	generateShip: function(){
+		var direction = Math.floor(Math.random() * 2);
+		var row, col;
+		
+		if(direction === 1){
+			//Generate horizontal ship
+			row = Math.floor(Math.random() * this.boardSize);
+			col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+			
+		}else{
+			//Generate veritcal ship
+			row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+			col = Math.floor(Math.random() * this.boardSize);
+		}
+		
+		var newShipLocations = [];
+		
+		for(var i = 0; i < this.shipLength; i++){
+			if(direction === 1){
+				// add location to array for new horizontal ship
+				newShipLocations.push(row + "" + (col + i));
+			}else{
+				//add location for new vertical ship
+				newShipLocations.push((row + i)+ "" +col);
+			}
+		}
+		return newShipLocations;
+	},
+	
+	collision: function(locations){
+		for(var i = 0; i < this.numShips; i++){
+			var ship = model.ships[i];
+			for(var j = 0; j< locations.length;j++){
+				if(ship.locations.indexOf(locations[j]) >=0){
+					return true;
+				}
+			}
+		}
+		return false;
 	}
-	
-	
 	
 };
 
@@ -74,7 +135,13 @@ var controller = {
 	processGuess: function(guess){
 		var location = parseGuess(guess);
 		if(location){
+			this.guessess++;
+			var hit = model.fire(location);
 			
+			if (hit && model.shipsSunk === model.numShips){
+				view.displayMessage("You sank all my battleships, in " + this.guessess + " guessess");
+				
+			}
 		}
 	}
 	
@@ -95,7 +162,7 @@ function parseGuess(guess){
 			if(isNaN(row) || isNaN(column)){
 				alert("oops, that isn't on the board.");
 				
-			}else if(row < 0 || row >= model.boardSize){
+			}else if(row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize){
 				alert("Oops, that's off the board!");
 			}else{
 				return row + column;
@@ -104,8 +171,37 @@ function parseGuess(guess){
 		}
 		return null;
 	}; //Left off on page 355 
+	
+function handleFireButton(){
+	
+	var guessInput = document.getElementById("guessInput");
+	var guess = guessInput.value;
+	controller.processGuess(guess);
+	guessInput.value = "";
+};
 
-view.displayMessage("HIT!!!");
-view.displayHit("00");
-view.displayHit("66");
-view.displayMiss("55");
+function init(){
+	var fireButton = document.getElementById("fireButton");
+	fireButton.onclick = handleFireButton;
+	
+	var guessInput = document.getElementById("guessInput");
+	guessInput.onkeypress = handleKeyPress;
+	model.generateShipLocations();
+	
+};
+
+function handleKeyPress(e){
+	var fireButton = document.getElementById("fireButton");
+	var key = e.keyCode;
+	
+	if(key === 13){
+		fireButton.click();
+		return false;
+	}
+};
+
+
+window.onload = init;
+
+
+
